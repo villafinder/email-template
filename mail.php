6fd3426069358cb7470ed0235f4e5abcef7e5e30
aeudes <adrien.eudes@gmail.com>
2014-08-16 12:22:22 +0800

    Mv CSS to inline

--- mail.php
+++ mail.php
@@ -1,7 +1,5 @@
 <?php
 $to  = 'adrien@villa-bali.com' . ', ';
-//$to .= 'masterjf@hotmail.com';
-//$to .= 'helene.pattin@infonie.fr';
 
 // subject
 $subject = 'Template Email Test';
@@ -14,9 +12,8 @@ $headers  = 'MIME-Version: 1.0' . "\r\n";
 $headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
 
 // Additional headers
-$headers .= 'To: Mary <james@villa-bali.com>, Kelly <masterjf@hotmail.com>' . "\r\n";
+$headers .= 'To: Adrien <adrien@villa-bali.com>' . "\r\n";
 $headers .= 'From: Villa-Bali.com <contact@villa-bali.com>' . "\r\n";
-$headers .= 'Cc: helene.pattin@infonie.fr' . "\r\n";
 
 // Mail it
 echo mail($to, $subject, $message, $headers);
